76479616c4d03acd923ccd625627e364187d160b
Guillaume Guimond <guillaume.guimond@orange.com>
2022-01-21 14:23:24 +0100

    new game memory part1

---
+++ memory/src/main.js
@@ -0,0 +1,20 @@
+import Phaser from 'phaser'
+
+import Game from './scenes/Game'
+import Preloader from './scenes/Preloader'
+
+
+const config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 0 }
+		}
+	},
+	scene: [Preloader, Game]
+}
+
+export default new Phaser.Game(config)
