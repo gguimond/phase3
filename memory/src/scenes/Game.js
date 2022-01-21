102e11bbdcb1c33d2e7f4ad3a0e05d99e2a6f419
Guillaume Guimond <guillaume.guimond@orange.com>
2022-01-21 16:59:51 +0100

    part 3

--- memory/src/scenes/Game.js
+++ memory/src/scenes/Game.js
@@ -1,5 +1,11 @@
 import Phaser from 'phaser'
 
+const level = [
+	[1, 0, 3],
+	[2, 4, 1],
+	[3, 4, 2]
+]
+
 export default class Game extends Phaser.Scene
 {
 	constructor()
@@ -21,21 +27,79 @@ export default class Game extends Phaser.Scene
 
         let xPer = 0.25
         let y = 150
-        for (let row = 0; row < 3; ++row)
+        for (let row = 0; row < level.length; ++row)
         {
-            for (let col = 0; col < 3; ++col)
+            for (let col = 0; col < level[row].length; ++col)
             {
                 /** @type {Phaser.Physics.Arcade.Sprite} */
                 const box = this.boxGroup.get(width * xPer, y, 'sokoban', 10)
                 box.setSize(64, 32)
                     .setOffset(0, 32)
-    
+                    .setData('itemType', level[row][col])
                 xPer += 0.25
             }
     
             xPer = 0.25
             y += 150
         }
+    }
+
+    /**
+ * 
+ * @param {Phaser.Physics.Arcade.Sprite} box 
+ */
+    openBox(box)
+    {
+        if (!box)
+        {
+            return
+        }
+
+        const itemType = box.getData('itemType')
+            
+        /** @type {Phaser.GameObjects.Sprite} */
+        let item
+
+        switch (itemType)
+        {
+            case 0:
+			item = this.itemsGroup.get(box.x, box.y)
+			item.setTexture('bear')
+			break
+
+            case 1:
+                item = this.itemsGroup.get(box.x, box.y)
+                item.setTexture('chicken')
+                break
+
+            case 2:
+                item = this.itemsGroup.get(box.x, box.y)
+                item.setTexture('duck')
+                break
+
+            case 3:
+                item = this.itemsGroup.get(box.x, box.y)
+                item.setTexture('parrot')
+                break
+
+            case 4:
+                item = this.itemsGroup.get(box.x, box.y)
+                item.setTexture('penguin')
+                break
+        }
+
+        box.setData('opened', true)
+
+        item.scale = 0
+        item.alpha = 0
+
+        this.tweens.add({
+            targets: item,
+            y: '-=50',
+            alpha: 1,
+            scale: 1,
+            duration: 500
+        })
     }
 
     create()
@@ -48,14 +112,59 @@ export default class Game extends Phaser.Scene
             .play('down-idle')    
 
         this.boxGroup = this.physics.add.staticGroup()
+		this.itemsGroup = this.add.group()
 
         this.createBoxes()
 
-        this.physics.add.collider(this.player, this.boxGroup)
+        this.physics.add.collider(
+            this.boxGroup,
+            this.player,
+            this.handlePlayerBoxCollide, // 👈 here
+            undefined,
+            this
+        )
 
     }
 
-    update(){
+    handlePlayerBoxCollide(player, box)
+    {
+        if (this.activeBox)
+        {
+            return
+        }
+
+        this.activeBox = box
+
+        this.activeBox.setFrame(9)
+    }
+
+    updateActiveBox()
+    {
+        if (!this.activeBox)
+        {
+            return
+        }
+
+        // get the distance here 👇
+        const distance = Phaser.Math.Distance.Between(
+            this.player.x, this.player.y,
+            this.activeBox.x, this.activeBox.y
+        )
+
+        if (distance < 64) // 👈 do nothing if still near
+        {
+            return
+        }
+
+        // return to using frame 10 when too far
+        this.activeBox.setFrame(10)
+
+        // and make activeBox undefined
+        this.activeBox = undefined
+    }
+
+    updatePlayer()
+    {
         const speed = 200
 
         if (this.cursors.left.isDown)
@@ -87,12 +196,28 @@ export default class Game extends Phaser.Scene
             this.player.play(`${direction}-idle`)
         }
 
+        const spaceJustPressed = Phaser.Input.Keyboard.JustUp(this.cursors.space)
+        if (spaceJustPressed && this.activeBox)
+        {
+            this.openBox(this.activeBox)
+
+            this.activeBox.setFrame(10)
+            this.activeBox = undefined
+        }
+    }
+
+    update(){
+        this.updatePlayer()
+
+        this.updateActiveBox()
+
         this.children.each(c => {
             /** @type {Phaser.Physics.Arcade.Sprite} */
             // @ts-ignore
             const child = c
-    
+
             child.setDepth(child.y)
         })
+
     }
 }
